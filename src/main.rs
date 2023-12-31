6dad7f43f9cbc2f4b2660aea6a99498832d3b474
yunamari <151783331+yunamari@users.noreply.github.com>
2023-12-31 16:29:03 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,53 @@
-use std::env;
+mod scan;
+
+use std::{env, io};
+use std::io::Write;
 use std::net::{SocketAddr, TcpStream};
 use std::time::Duration;
+use crate::scan::scan_ports;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        eprintln!("Usage: {} [options]", args[0]);
-        std::process::exit(1);
-    }
-
     let mut ip = String::new();
-    let mut port: Option<u16> = None;
     let mut scan_all = false;
     let mut show_graphic = false;
     let mut verbose = false;
 
-    for i in 1..args.len() {
-        match args[i].as_str() {
-            "-i" | "--ip" => {
-                ip = args[i + 1].clone();
+
+    if args.len() < 2 {
+        let mut cmd = String::new();
+        print!("Enter a command, example:\n\tscan <ip> <port/all> <tcp/udp/all>\n\tping <ip>\n\nEnter your cmd: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut cmd).expect("An error occurred while reading your command.");
+
+        let cmd = cmd.trim();
+
+        if cmd.is_empty() {
+            println!("Can you specify a command?");
+        }
+
+        let cmd_eq: Vec<&str> = cmd.split_whitespace().collect();
+
+        if cmd_eq.get(0) == Some(&"scan") {
+            if let (Some(ip), Some(port)) = (cmd_eq.get(1), cmd_eq.get(2)) {
+                let ip = ip.to_string();
+                let port = port.to_string();
+
+                if port.eq_ignore_ascii_case("All") {
+                    scan_all = true;
+                    scan_ports(ip, port, scan_all);
+                }
             }
-            "-p" | "--port" => {
-                port = Some(args[i + 1].parse().expect("Invalid port number"));
+            else {
+                println!("Invalid format for 'scan' command");
             }
-            "-a" | "--all" => {
-                scan_all = true;
-            }
-            "-G" | "--graphic" => {
-                show_graphic = true;
-            }
-            "-v" | "--verbose" => {
-                verbose = true;
-            }
-            "-h" | "--help" => {
-                println!("Help message here");
-                std::process::exit(0);
-            }
-            _ => {}
+        }
+
+
+        else {
+            println!("Unknown command or invalid format.");
         }
     }
-
 }
 
