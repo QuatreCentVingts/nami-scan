4f2b3a51a52826b258041f49a7246d0abb75fb61
yunamari <151783331+yunamari@users.noreply.github.com>
2023-12-31 18:10:27 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,58 @@ mod scan;
 
 use std::{env, io};
 use std::io::Write;
-use std::net::{SocketAddr, TcpStream};
-use std::time::Duration;
+use std::net::SocketAddr;
 use crate::scan::scan_ports;
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut ip = String::new();
     let mut scan_all = false;
-    let mut show_graphic = false;
-    let mut verbose = false;
-
 
     if args.len() < 2 {
-        let mut cmd = String::new();
-        print!("Enter a command, example:\n\tscan <ip> <port/all> <tcp/udp/all>\n\tping <ip>\n\nEnter your cmd: ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut cmd).expect("An error occurred while reading your command.");
+        loop {
+            let mut cmd = String::new();
+            println!("Enter a command, example:
+            <scan> <ip>:<port/all>
+            <ping> <ip>
+            <conn> <processus/all>");
 
-        let cmd = cmd.trim();
+            print!("\nEnter your cmd: ");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut cmd).expect("An error occurred while reading your command.");
 
-        if cmd.is_empty() {
-            println!("Can you specify a command?");
-        }
+            let cmd = cmd.trim();
 
-        let cmd_eq: Vec<&str> = cmd.split_whitespace().collect();
+            if cmd.is_empty() {
+                println!("Can you specify a command?");
+            }
 
-        if cmd_eq.get(0) == Some(&"scan") {
-            if let (Some(ip), Some(port)) = (cmd_eq.get(1), cmd_eq.get(2)) {
-                let ip = ip.to_string();
-                let port = port.to_string();
+            let cmd_eq: Vec<&str> = cmd.split_whitespace().collect();
 
-                if port.eq_ignore_ascii_case("All") {
-                    scan_all = true;
-                    scan_ports(ip, port, scan_all);
+            if cmd_eq.get(0) == Some(&"scan") {
+                if let Some(ip_port) = cmd_eq.get(1) {
+                    let ip_port_parts: Vec<&str> = ip_port.split(':').collect();
+
+                    if let (Some(ip), Some(port)) = (ip_port_parts.get(0), ip_port_parts.get(1)) {
+                        let ip_port_str = format!("{}:{}", ip, port);
+                        let socket_addr: SocketAddr = ip_port_str.parse().expect("Invalid IP:Port format");
+
+                        if port.eq_ignore_ascii_case("all") {
+                            scan_all = true;
+                        }
+
+                        scan_ports(socket_addr.ip().to_string(), socket_addr.port().to_string(), scan_all);
+                    } else {
+                        println!("Invalid format for 'scan' command");
+                    }
+                } else {
+                    println!("Invalid format for 'scan' command");
                 }
+            } else {
+                println!("Unknown command or invalid format.");
             }
-            else {
-                println!("Invalid format for 'scan' command");
-            }
+            println!("\n");
         }
+    } else {
 
-
-        else {
-            println!("Unknown command or invalid format.");
-        }
     }
 }
-
