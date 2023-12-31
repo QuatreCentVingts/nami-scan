ed78f736c2dd8b12ad694346103974281c76b289
yunamari <151783331+yunamari@users.noreply.github.com>
2023-12-31 17:02:45 +0100

    Update scan.rs

--- src/scan.rs
+++ src/scan.rs
@@ -1,50 +1,22 @@
-use std::net::{SocketAddr, TcpStream, UdpSocket};
+use std::net::TcpStream;
 use std::time::Duration;
 
 pub fn scan_ports(ip: String, port: String, scan_all: bool) {
     println!("Scanning ports for {}", ip);
 
     if scan_all {
-        for port_number in 1..=65535 {
-            let addr = format!("{}:{}", ip, port_number);
-            match addr.parse::<SocketAddr>() {
-                Ok(socket_addr) => {
-                    if tcp_port_open(socket_addr) {
-                        println!("Port {} is open (TCP)", port_number);
-                    } else if udp_port_open(socket_addr) {
-                        println!("Port {} is open (UDP)", port_number);
-                    }
-                }
-                Err(e) => eprintln!("Error parsing address: {}", e),
+        for port in 1..=65535 {
+            let addr = format!("{ip}:{port}");
+            match TcpStream::connect_timeout(&addr.parse().unwrap(), Duration::from_secs(0)) {
+                Ok(_) => println!("Port {} is open", port),
+                Err(_) => println!("port {} is closed", port),
             }
         }
-    } else {
-        let addr = format!("{}:{}", ip, port);
-        match addr.parse::<SocketAddr>() {
-            Ok(socket_addr) => {
-                if tcp_port_open(socket_addr) {
-                    println!("Port {} is open (TCP)", port);
-                } else if udp_port_open(socket_addr) {
-                    println!("Port {} is open (UDP)", port);
-                } else {
-                    println!("Port {} is closed", port);
-                }
-            }
-            Err(e) => eprintln!("Error parsing address: {}", e),
+    }else {
+        let addr = format!("{ip}:{port}");
+        match TcpStream::connect_timeout(&addr.parse().unwrap(), Duration::from_secs(0)) {
+            Ok(_) => println!("Port {} is open", port),
+            Err(_) => println!("port {} is closed", port),
         }
-    }
-}
-
-fn tcp_port_open(addr: SocketAddr) -> bool {
-    match TcpStream::connect_timeout(&addr, Duration::from_secs(0)) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
-}
-
-fn udp_port_open(addr: SocketAddr) -> bool {
-    match UdpSocket::bind(addr) {
-        Ok(_) => true,
-        Err(_) => false,
     }
 }
